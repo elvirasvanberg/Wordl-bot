8d7bf0bf13a9e252c9e2f9ba19849781bff3f9b9
Elvira <elvira.svanberg@gmail.com>
2022-04-25 15:39:16 +0200

    word scores

--- wordl_bot.py
+++ wordl_bot.py
@@ -18,8 +18,6 @@ def char_frequency(list):
 
     return dict
 
-print(char_frequency(word_list))
-
 def relative_char_frequency():
     char_frequency_dict = char_frequency(word_list)
     values = char_frequency_dict.values()
@@ -27,9 +25,35 @@ def relative_char_frequency():
     
     char_frequency_list = list(char_frequency_dict.items())
     
-    for c in range(len(char_frequency_list)):
-        char_frequency_list[c][1] == char_frequency_list[c][1]/total_char_num
+    relative_char_frequency_list = []
+    for element in char_frequency_list:
+        element2 = (element[0], element[1]/total_char_num)
+        relative_char_frequency_list.append(element2)
     
-    return char_frequency_list
+    char_by_relative_frequency = sorted(relative_char_frequency_list, key = lambda x: x[1], reverse = True)
 
-print(relative_char_frequency())
+    return char_by_relative_frequency
+
+def score(word):
+    char_frequency_list = relative_char_frequency()
+
+    score = 0
+    for letter in word:
+        i = 0
+        if letter != char_frequency_list[i][0]:
+            i += 1
+        score += char_frequency_list[i][1]
+    
+    # take points off if there are multiple of the same letter
+    for a in range(word_length):
+        for b in range(word_length):
+            if word[a] == word[b] and a != b:
+                score -= 0.05
+
+    return score
+
+def word_by_score():
+    word_list_and_score = [(word, score(word)) for word in word_list]
+
+    word_list_by_score = sorted(word_list_and_score, key = lambda x: x[1], reverse = True)
+    return word_list_by_score[:20]
