7012e63bed3b467bc0fd1689c734ff6cd5cc66ab
Elvira <elvira.svanberg@gmail.com>
2022-05-20 09:47:44 +0200

    småfix nr. 3

--- filter_words.py
+++ filter_words.py
@@ -1,27 +1,25 @@
-from word_score import word_by_score
-
 with open('wordle-answers-alphabetical.txt', 'r') as file:
     possible_answers = file.read().splitlines()
 
 def compare_words(query_word, answer_word):
-    res = []
+    comparison_code = []
     pos_not_found = []
     letters_not_found = []
     
     for (i, letter1), letter2 in zip(enumerate(query_word), answer_word):
         if letter1 == letter2:
-            res.append('2')
+            comparison_code.append('2')
         else:
-            res.append('0')
+            comparison_code.append('0')
             pos_not_found.append(i)
             letters_not_found.append(letter2)
     
     for i in pos_not_found:
         if query_word[i] in letters_not_found:
-            res[i] = '1'
+            comparison_code[i] = '1'
             letters_not_found.remove(query_word[i])
     
-    return "".join(res)
+    return "".join(comparison_code)
 
 def filter_words(possible_words, word, comparison_code):
     possible_words_upd = []
@@ -31,4 +29,4 @@ def filter_words(possible_words, word, comparison_code):
         else:
             pass
         
-    return word_by_score(possible_words_upd)
+    return possible_words_upd
--- wordle_bot.py
+++ wordle_bot.py
@@ -1,13 +1,15 @@
 from filter_words import filter_words
+from word_score import word_by_score
 
 with open('wordle-answers-alphabetical.txt', 'r') as file:
     possible_answers = file.read().splitlines()
 
 def guess(possible_words, word, comparison_code):
     guess_list = filter_words(possible_words, word, comparison_code)
-    
+    guess_list_sorted = word_by_score(guess_list)
+
     if len(guess_list) == 0:
         print('Det finns inga möjliga ord i ordlistan som uppfyller kraven')
         return []
     else:
-        return guess_list[0][0]
+        return guess_list_sorted[0][0]
